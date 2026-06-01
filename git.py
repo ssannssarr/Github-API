c9541298cae0736c3e901796c512004978e5921b
ssannssarr <ssantanusarkar8@gmail.com>
2026-06-01 22:20:43 +0530

    modified formatting

--- git.py
+++ git.py
@@ -14,11 +14,11 @@ For command list type "/help"
 ^_^
 """
 HELP_TEXT = """
-+---------------------------------------------------------------+
-| /usr_qry : This cmd will open a user detail grabbing function |
+|---------------------------------------------------------------|
+| /qry : This cmd will open a user detail grabbing function |
 | /help    : To open this help text                             |
 | /exit    : To exit this py app                                |
-+---------------------------------------------------------------+ 
+|---------------------------------------------------------------|
 """
 
 print(WELCOME_TEXT)
@@ -29,7 +29,7 @@ try:
         usr_in = input(">> ").strip().lower()
         if usr_in == "/help":
             print(HELP_TEXT)
-        elif usr_in == "/usr_qry":
+        elif usr_in == "/qry":
             gh.usr_info()
         elif usr_in != "/exit":
             print("I think that cmd is not available T.T")
